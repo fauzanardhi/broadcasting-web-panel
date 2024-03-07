99fda00bd4cf00890a2a22c7210cbbd9dbc58077
dhlah <aminahnaf03@gmail.com>
2024-03-07 07:42:25 +0700

    toast update

--- components/Providers/ToastProvider.jsx
+++ components/Providers/ToastProvider.jsx
@@ -4,7 +4,7 @@ import { Toaster } from "sonner";
 export default function ToastProvider({ children }) {
   return (
     <div>
-      <Toaster position="bottom-left" />
+      <Toaster position="top-right" />
       {children}
     </div>
   );
